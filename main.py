106f14eb2954e5db9fda2ebd6e3f2d3133779063
aanginer <96923304+aanginer@users.noreply.github.com>
2022-03-22 22:57:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 def liststr(l: list):
   string = ""
   for i in l:
@@ -7,26 +5,19 @@ def liststr(l: list):
 
   return string
 
-def toBinary(a):
-  l,m=[],[]
-  for i in a:
-    l.append(ord(i))
-  for i in l:
-    m.append(int(bin(i)[2:]))
-  return liststr(m)
+def toBinary(text, encoding='utf-8', errors='surrogatepass'):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+    return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-def binarystring(s):
-    return ''.join(chr(int(s[i*8:i*8+8],2)) for i in range(len(s)//8))
+def binarystring(bits, encoding='utf-8', errors='surrogatepass'):
+    n = int(bits, 2)
+    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
 
+def encode(string):
+  encoded = toBinary(string)
+  encoded = encoded.replace('00','erydta').replace("01","uicnajdja").replace("10","afsuid").replace("11","iajcjdhcnaberyr")
+  return encoded
 
-class Encoder:
-    def __init__(self,data: dict,name: str,game: str):
-        self.first = True
-        self.data_str = str(data)
-        self.data = data
-        self.encoded = ''
-        self.decoded = ''
-    
-    def encode(self):
-        self.encoded = binarystring(toBinary(self.data_str).replace('0','0_p').replace('1','0').replace('0_p','1'))
-        self.encoded.replace('a')
+def decode(string: str):
+  decoded = string.replace('erydta','00').replace("uicnajdja","01").replace("afsuid","10").replace("iajcjdhcnaberyr","11")
+  return binarystring(decoded)
